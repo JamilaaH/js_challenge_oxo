bfb57dcab25a024a4421e1c35c4b6c018049fc66
Jamila <jaja_mimi@hotmail.com>
2021-02-17 19:55:31 +0100

    fin

--- public/js/main.js
+++ public/js/main.js
@@ -4,15 +4,17 @@ let btnRestart = document.querySelector('.restart')
 let player1 = true ;
 let player2 = false ;
 
+// le jeu commence
 let gameActive = true;
-tour.innerHTML = "C'est parti"
+tour.innerHTML = "C'est parti <br> Au tour de joueur X"
+
 // ECRIRE X ET O 
 caseCliquable.forEach(element => {
 
     element.addEventListener(`click` , () => {
 
         let roundWon = false;
-        
+        //placer les X et O
         if (gameActive) {
             if (player1 && element.innerText == ``) {
                 element.innerText = `X`
@@ -38,7 +40,8 @@ caseCliquable.forEach(element => {
             let c1 = document.getElementById(`c-1`).innerText
             let c2 = document.getElementById(`c-2`).innerText
             let c3 = document.getElementById(`c-3`).innerText
-    
+
+            // tableau avec ttes les combinaisons gagnantes
             let tableauGagnant = [
                 [a1,a2,a3],
                 [b1,b2,b3],
@@ -49,14 +52,16 @@ caseCliquable.forEach(element => {
                 [a1,b2,c3],
                 [a3,b2,c1]
             ];
-    
+            
+            let tab = [a1,a2, a3, b1, b2, b3, c1, c2, c3]
+
+            //Vérifier si chaque ligne ou diagonal est fait
             for (let index = 0; index <= 7; index++) {
     
                 let position = tableauGagnant[index]
                 let position1 = position[0]
                 let position2 = position[1]
                 let position3 = position[2]  
-    
                 if (position1 == `X` && position2 == `X` && position3 == `X`) {
                     tour.innerHTML ='X a gagné'
                     console.log(`X a gagné`);
@@ -74,12 +79,23 @@ caseCliquable.forEach(element => {
                     return false;
     
                 }
+                // fin du round quand un joueur gagne
                 if (roundWon) {
                     gameActive = false
                     return false;
-                }
+
+                } 
+
             }
-    
+            
+            //cas d'egalité 
+            let roundDraw =! tab.includes("");
+            if (roundDraw) {
+                tour.innerHTML = "match nul"
+                gameActive  = false
+                btnRestart.style = "display: flex"
+                return false
+            }
         }
     })
 });
